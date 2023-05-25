7ff0285012ace1df77fdaf0ddae9a79e4221c63b
Vinicius Tanaka <vi_massaru.tanaka@hotmail.com>
2023-05-25 16:03:10 -0300

    refatoração-de-pastas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import  { ThemeProvider } from '@mui/material';
 import theme from './configs/theme';
-import SideNav from './components/navigation/Sidenav';
+// import SideNav from './components/navigation/Sidenav';
 // import Login from './components/Login';
 import GlobalStyles from './styles/GlobalStyles';
+import HeaderNav from './components/HeaderNav'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SideNav />
+      <HeaderNav />
+      {/* <SideNav /> */}
       {/* <Login /> */}
     </ThemeProvider>
   );
