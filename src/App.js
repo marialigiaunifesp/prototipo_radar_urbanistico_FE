62224ff0a5206d21da916e193962da485dda6023
Israel Rocha <israel.rexc@yahoo.com>
2023-06-12 15:18:58 -0300

    Sidenav correctly import path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import  { ThemeProvider } from '@mui/material';
 import theme from './configs/theme';
-// import SideNav from './components/navigation/Sidenav';
+// import SideNav from './components/Sidenav';
 // import Login from './components/LoginForm';
 import GlobalStyles from './styles/GlobalStyles';
 import HeaderNav from './components/HeaderNav'
