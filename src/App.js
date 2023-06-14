60ca36a21e33aef3db8389fb987ddc625cc7529d
Tris Costa <bia.irineu14@gmail.com>
2023-06-13 22:17:26 -0300

    changed app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import { ThemeProvider } from '@mui/material';
 import theme from './configs/theme';
-<<<<<<< HEAD
-// import SideNav from './components/Sidenav';
-// import Login from './components/LoginForm';
-=======
 import Login from './components/LoginForm';
->>>>>>> develop
 import GlobalStyles from './styles/GlobalStyles';
 import Formulario from './components/Formulario';
 import HeaderNav from './components/HeaderNav'
